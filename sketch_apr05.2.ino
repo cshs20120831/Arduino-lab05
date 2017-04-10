56b07857d2f3bec7f81f8d300295a1e8b017c122
cshs20120831 <cshs20120831@gmail.com>
2017-04-10 19:20:50 +0800

    4x4鍵盤輸入在七段顯示器顯示

---
+++ sketch_apr05.2.ino
@@ -0,0 +1,53 @@
+#include <Keypad.h>
+#include "SevSeg.h"
+SevSeg sevseg;
+const byte ROWS = 4; // 4 Rows
+const byte COLS = 4; // 4 Columns
+char keys[ROWS][COLS] = 
+{
+  {'0', '1', '2', '3'}, {'4','5','6', '7'},
+  {'8', '9', 'A', 'B'}, {'C', 'D', 'E', 'F'}
+};
+byte rowPins[ROWS] = {5, 4, 3, 2};
+byte colPins[COLS] = {9, 8, 7, 6};
+Keypad keypad = Keypad( makeKeymap(keys), rowPins,colPins, ROWS, COLS );
+void setup() 
+{
+  Serial.begin(9600); 
+  byte numDigits = 1;
+  byte digitPins[] = {A2};
+  byte segmentPins[] = {11, 10, A3, A1, A0, 12, 13, A4};
+  sevseg.begin(COMMON_CATHODE, numDigits, digitPins,segmentPins);
+}
+void loop()
+{
+  char key;
+  key = keypad.getKey();
+
+  int a;
+ 
+  if (key != NO_KEY)
+  {
+    if(key >='A' && key <='F')
+     {
+      a = key-'A';
+      } 
+    else
+    {
+      a = key-'0';
+      }
+    Serial.println(key);
+    sevseg.setNumber(a,1);
+    sevseg.refreshDisplay();
+  
+  }
+  else
+  {
+    sevseg.refreshDisplay();
+
+    
+  }
+
+
+
+}
